2b0b89d6a372a7c2f69a41f62575728a27271780
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-23 22:15:03 +0300

    Add state variables to sidebars page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
 
 const Sidebar = () => {
+  const { chats, setSelectedChat, theme, setTheme, user } = useAppContext();
+  const [search, setSearch] = useState("");
   return <div>Sidebar</div>;
 };
 
