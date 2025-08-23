0c32fd6574446109f08f1b233de3f6b2c525420f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-23 14:08:33 +0300

    Make a route for ChatBox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import { Sidebar } from "lucide-react";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
 
 const App = () => {
   return (
     <>
       <div>
         <Sidebar />
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+        </Routes>
       </div>
     </>
   );
