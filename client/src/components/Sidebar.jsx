6316b4382d3c618b7408e902ac29a85360046cd9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-24 00:44:44 +0300

    Finish the sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,12 +3,16 @@ import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import moment from "moment";
 
-const Sidebar = () => {
+const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
   const { chats, setSelectedChat, theme, setTheme, user, navigate } =
     useAppContext();
   const [search, setSearch] = useState("");
   return (
-    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
+    <div
+      className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 ${
+        !isMenuOpen && "max-md:-translate-x-full"
+      }`}
+    >
       {/* Logo */}
       <img
         src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
@@ -43,6 +47,11 @@ const Sidebar = () => {
           )
           .map((chat) => (
             <div
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
               key={chat._id}
               className="p-2 px-4 dark:bg-[#57317c]/10 border border-gray-300 dark:border-[#80609f]/15 rounded-md cursor-pointer flex justify-between group"
             >
@@ -68,6 +77,7 @@ const Sidebar = () => {
       <div
         onClick={() => {
           navigate("/community");
+          setIsMenuOpen(false);
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -84,6 +94,7 @@ const Sidebar = () => {
       <div
         onClick={() => {
           navigate("/credits");
+          setIsMenuOpen(false);
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -126,6 +137,7 @@ const Sidebar = () => {
         )}
       </div>
       <img
+        onClick={() => setIsMenuOpen(false)}
         src={assets.close_icon}
         className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert"
         alt=""
