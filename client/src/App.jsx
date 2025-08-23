806f41c41e6ecd037f69ceaf3f788c4ffe540327
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-23 14:17:46 +0300

    Fix import issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
-import { Sidebar } from "lucide-react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ChatBox from "./components/ChatBox";
 import Credits from "./pages/Credits";
 import Community from "./pages/Community";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
-      <div className="flex h-screen w-screen">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<ChatBox />} />
-          <Route path="/credits" element={<Credits />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
