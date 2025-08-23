5e8732e10dcb7f953150df8cdc0fc01f9ad760c8
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-23 23:09:07 +0300

    Add a new chat button

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,13 +6,18 @@ const Sidebar = () => {
   const { chats, setSelectedChat, theme, setTheme, user } = useAppContext();
   const [search, setSearch] = useState("");
   return (
-    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f] backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
+    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
       {/* Logo */}
       <img
         src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
         alt=""
-        className="w-full max-w-48a"
+        className="w-full max-w-48"
       />
+      {/* New Chat Button */}
+      <button className="flex items-center justify-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#a456f7] to-[#3d81f6] text-sm rounded-md cursor-pointer">
+        <span className="mr-2 text-xl">+</span> New Chat
+      </button>
+      {/* Search Conversations */}
     </div>
   );
 };
