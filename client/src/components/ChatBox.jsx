083fc3a553211f7376ab4192f1c786fc5f134503
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-24 09:44:02 +0300

    Add state variables

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
 
 const ChatBox = () => {
+  const { selectedChat, theme } = useAppContext();
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return <div>ChatBox</div>;
 };
 
