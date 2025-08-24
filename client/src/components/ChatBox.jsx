8da3b1af451b615a3bf841a6c718d43576fc188b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-24 11:59:59 +0300

    Make it scroll to the bottom when with other recent chats

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import Message from "./Message";
 
 const ChatBox = () => {
+  const containerRef = useRef();
   const { selectedChat, theme } = useAppContext();
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -21,10 +22,19 @@ const ChatBox = () => {
     }
   }, [selectedChat]);
 
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
   return (
     <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
       {/* Chat Messages */}
-      <div className="flex-1 mb-5 overflow-y-scroll">
+      <div ref={containerRef} className="flex-1 mb-5 overflow-y-scroll">
         {messages.length === 0 && (
           <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
             <img
