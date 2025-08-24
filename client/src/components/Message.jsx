87695b1a7ff8f4ab85cd6268a3cbb001bcc9859c
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-24 10:20:50 +0300

    Add user message box

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
-const Message = () => {
-  return <div>Message</div>;
+const Message = ({ message }) => {
+  return (
+    <div>
+      {message.role === "user" ? (
+        <div className="flex items-start justify-end my-4 gap-2">
+          <div className="flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#57317c]/30 border border-[#80609f]/30 rounded-md max-w-2xl">
+            <p className="text-sm dark:text-primary">{message.content}</p>
+            <span className="text-xs text-gray-400 dark:text-[#b1a6c0]">
+              {message.timestamp}
+            </span>
+          </div>
+          <img src={assets.user_icon} className="w-8 rounded-full" alt="" />
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Message;
