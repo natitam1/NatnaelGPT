164aa8eb7e311a7cfa8c10b5eeae0499e3c98821
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-25 11:02:56 +0300

    Set up the credits page

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { dummyPlans } from "../assets/assets";
+import Loading from "./Loading";
 
 const Credits = () => {
   const [plans, setPlans] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  return <div>Credits</div>;
+  const fetchPlans = async () => {
+    setPlans(dummyPlans);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPlans();
+  }, []);
+
+  if (loading) return <Loading />;
+  return (
+    <div className="max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <h2 className="text-3xl font-semibold text-center mb-10 xl:mt-30 text-gray-800 dark:text-white">
+        Credit Plans
+      </h2>
+    </div>
+  );
 };
 
 export default Credits;
