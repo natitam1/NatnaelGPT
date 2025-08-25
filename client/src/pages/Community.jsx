8a95473a7aea6f5b5101005fca5ff01fc67db32a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-25 09:49:53 +0300

    Set up community page where users can share images

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyPublishedImages } from "../assets/assets";
+import Loading from "./Loading";
 
 const Community = () => {
-  return <div>Community</div>;
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchImages = async () => {
+    setImages(dummyPublishedImages);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  if (loading) return <Loading />;
+  return (
+    <div className="p-6 py-12 xl:px-12 2xl:px-20 w-full mx-auto h-full overflow-y-scroll">
+      <h2 className="text-xl font-semibold mb-6 text-gray-800 dark:text-purple-100">
+        Community Images
+      </h2>
+    </div>
+  );
 };
 
 export default Community;
