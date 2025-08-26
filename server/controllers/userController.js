a521e3e1f56faf576f833be5e9d07322d2eafdfc
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-26 17:31:46 +0300

    Create API to login user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
-
+import bcrypt from "bcryptjs";
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -21,6 +21,25 @@ export const registerUser = async (req, res) => {
 
     const token = generateToken(user._id);
     res.json({ success: true, token });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to login user
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch) {
+        const token = generateToken(user._id);
+        return res.json({ success: true, token });
+      }
+    }
+    return res.json({ success: false, message: "Invalid email or password" });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
