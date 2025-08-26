4c5733884b9e8b5175fa3e039f8105f9d148f83e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-26 17:13:26 +0300

    Create API to register user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,3 +7,21 @@ const generateToken = (id) => {
     expiresIn: "30d",
   });
 };
+
+// API to register user
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    const token = generateToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
