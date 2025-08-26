d790a39c07f96138b7969e1e4bd3394da7cbb8e8
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-26 17:02:16 +0300

    Generate JWT

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
