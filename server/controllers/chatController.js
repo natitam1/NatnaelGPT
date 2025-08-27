eedf00eed6c63515da5e7acc6b9debbbd42042b7
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-27 12:19:05 +0300

    Create an API Controller for getting all chats

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -15,25 +15,18 @@ export const createChat = async (req, res) => {
     await Chat.create(chatData);
     res.json({ success: true, message: "Chat created" });
   } catch (error) {
-    res.json({ success: false, error: error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
-// API Controller for creating a new chat
-export const createChat = async (req, res) => {
+// API Controller for getting all chats
+export const getChats = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const chatData = {
-      userId,
-      messages: [],
-      name: "New Chat",
-      userName: req.user.name,
-    };
-
-    await Chat.create(chatData);
-    res.json({ success: true, message: "Chat created" });
+    const chats = await Chat.find({ userId }).sort({ updatedAt: -1 });
+    res.json({ success: true, chats });
   } catch (error) {
-    res.json({ success: false, error: error.message });
+    res.json({ success: false, message: error.message });
   }
 };
