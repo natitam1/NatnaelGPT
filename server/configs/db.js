876e4b68045b507cf8c67df25289cc88f6188379
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-27 11:54:55 +0300

    Create a Chats model

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/quickgpt`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
   } catch (error) {
     console.log(error.message);
   }
