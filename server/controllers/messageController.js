acd3cfa21f60f4ddc4ac779142bd10011cb2b500
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-27 22:59:52 +0300

    Create text-based AI chat messages controller

---
+++ server/controllers/messageController.js
@@ -0,0 +1,38 @@
+import Chat from "../models/Chat.js";
+import User from "../models/user.js";
+
+// Text-based AI Chat Message Controller
+export const textMessageController = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { chatId, prompt } = req.body;
+
+    const chat = await Chat.findOne({ userId, _id: chatId });
+    chat.messages.push({
+      role: "user",
+      content: prompt,
+      timestamp: Date.now(),
+      isImage: false,
+    });
+    const { choices } = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+    const reply = {
+      ...choices[0].message,
+      isImage: false,
+      timestamp: Date.now(),
+    };
+    res.json({ success: true, message: reply });
+    chat.messages.push(reply);
+    await chat.save();
+    await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
