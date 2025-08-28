629e0af39c3aaf01d27af729fdf55879f6b1e30a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-28 10:58:08 +0300

    Add check credits for specific user

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -7,6 +7,10 @@ import axios from "axios";
 export const textMessageController = async (req, res) => {
   try {
     const userId = req.user._id;
+    // Check credits
+    if (req.user.credits < 2) {
+      return res.json({ success: false, message: "Insufficient credits" });
+    }
     const { chatId, prompt } = req.body;
 
     const chat = await Chat.findOne({ userId, _id: chatId });
